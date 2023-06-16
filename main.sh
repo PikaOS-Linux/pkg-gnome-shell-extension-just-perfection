6414dd0ef5e49aeba999dcf471e0e17ec083d2ba
ferrreo <harderthanfire@gmail.com>
2023-06-16 19:02:00 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,18 +1,11 @@
 DEBIAN_FRONTEND=noninteractive
 
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 git clone -b 24.0 https://gitlab.gnome.org/jrahmatzadeh/just-perfection ./gnome-shell-extension-just-perfection
 cp -rvf ./debian ./gnome-shell-extension-just-perfection/
 cd ./gnome-shell-extension-just-perfection
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
